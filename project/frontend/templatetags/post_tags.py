7cee0c510f9697805576bcf439c7824214189c39
Afraiven <filip.antoniak99@gmail.com>
2023-04-01 23:21:29 +0200

    czerwone powiadomienia

--- project/frontend/templatetags/post_tags.py
+++ project/frontend/templatetags/post_tags.py
@@ -11,3 +11,15 @@ def notification(context):
         return powiadomienia.count()
     else:
         return "0"
+
+@register.simple_tag(takes_context=True)
+def notification_class(context):
+    user = context['user']
+    if user.is_authenticated:
+        powiadomienia = Powiadomienia.objects.filter(user=user)
+        if powiadomienia.count() > 0:
+            return "redcolor"
+        else: 
+            return ""
+    else:
+        return ""
